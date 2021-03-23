e24f0d1dc487e5e12e4ed5e77b6aa2e4c3cf1fb1
Qasim Hafeez <qasimh556@gmail.com>
2021-03-23 12:47:34 +0400

    mount routes

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const dotenv = requite("dotenv");
 
+// Routes
+const bootcamps = require("./routes/bootcamps");
+
 // Load env variables
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+// Mount Routers
+app.use("/api/v1/bootcamps", bootcamps);
+
+// Server Config
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
