bbb1fdc99b839b180d0495c3c81cf95e48a29323
Qasim Hafeez <qasimh556@gmail.com>
2021-03-24 12:40:59 +0400

    geocoder middleware to handle address

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const geocoder = require("../utils/geocoder");
 
 // Schema
 const BootCampSchema = new mongoose.Schema({
@@ -43,11 +44,9 @@ const BootCampSchema = new mongoose.Schema({
     type: {
       type: String,
       enum: ["Point"],
-      required: true,
     },
     coordinates: {
       type: [Number],
-      required: true,
       index: "2dsphere",
     },
     formattedAddress: String,
@@ -102,9 +101,29 @@ const BootCampSchema = new mongoose.Schema({
   },
 });
 
-// Middleware -- Create Slug from the name
+// -----------Middlewares--------------//
+// Create Slug from the name
 BootCampSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode and save location field
+BootCampSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].state,
+    zipcode: loc[0].zipcode,
+    country: loc[0].country,
+  };
+
+  // Do not save address in DB
+  this.address = undefined;
   next();
 });
 
