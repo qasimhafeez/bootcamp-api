7d9b439a22413e20b1b3824eaf11da102816fb9d
Qasim Hafeez <qasimh556@gmail.com>
2021-03-24 12:00:55 +0400

    more validations

--- middleware/error.js
+++ middleware/error.js
@@ -1,7 +1,31 @@
-const errorHandler = (err, req, res, next) => {
-  console.log(err.stack.red);
+const ErrorResponse = require("../utils/errorResponse");
 
-  res.status(500).json({ success: false, error: err.message });
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.statusCode = err.statusCode;
+  error.message = error.message;
+
+  console.log(err.stack.red);
+  // Mongoose bad objectID
+  if (err.name === "CastError") {
+    const message = `${err.value} id not found`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose Duplicate ID Error
+  if (err.code === 11000) {
+    const message = `${err.value} id already exists`;
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+  res
+    .status(error.statusCode || 500)
+    .json({ success: false, error: error.message || "Server Error" });
 };
 
 module.exports = errorHandler;
