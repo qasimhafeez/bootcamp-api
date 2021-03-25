d13f70915f7019adce437bda683579dd58fc34a2
Qasim Hafeez <qasimh556@gmail.com>
2021-03-25 14:56:45 +0400

    initial User Model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+// Schema
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Add a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Add an email"],
+    unique: true,
+    match: [
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+      "Please enter a valid email",
+    ],
+  },
+  role: {
+    type: String,
+    enum: ["user", "publisher"],
+    default: "user",
+  },
+  password: {
+    type: String,
+    required: [true, "Add a password"],
+    minlength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
