1a422269b366eac2dad0915b8c73f3642f9987e0
Qasim Hafeez <qasimh556@gmail.com>
2021-03-26 00:28:11 +0400

    auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -71,3 +71,14 @@ const sendTokenReponse = (user, statusCode, res) => {
     .cookie("cookie-name-token", token, options)
     .json({ success: true, token });
 };
+
+// @desc      Get current logged in user
+// @route     GET /api/v1/auth/me
+// @access    Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
