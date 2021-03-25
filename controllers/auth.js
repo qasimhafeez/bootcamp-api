fc8ca48ba63f46c8674b62647d5bae9df4442dcd
Qasim Hafeez <qasimh556@gmail.com>
2021-03-25 16:06:34 +0400

    send token after successful login

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,5 +16,38 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  res.status(201).json({ success: true, data: user });
+  // Create Token
+  const token = user.getSignedJwtToken();
+
+  res.status(201).json({ success: true, token });
+});
+
+// @desc      Login User
+// @route     POST /api/v1/auth/login
+// @access    Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Quick validation for email and password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and password", 400));
+  }
+
+  // Check for user and include password as well
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid Credentials", 401));
+  }
+
+  // Compare the plain password with hashed one
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid Credentials", 401));
+  }
+
+  // Create Token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
 });
