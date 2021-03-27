ffa89bbc33512a15f6cb5f43c0a477b39d82e00e
Qasim Hafeez <qasimh556@gmail.com>
2021-03-27 13:16:40 +0400

    mount users routes

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const errorHandler = require("./middleware/error");
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
 const auth = require("./routes/auth");
+const users = require("./routes/users");
 
 // DB
 connectDB();
@@ -39,6 +40,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
 app.use("/api/v1/auth", auth);
+app.use("/api/v1/auth/users", users);
 
 app.use(errorHandler);
 
