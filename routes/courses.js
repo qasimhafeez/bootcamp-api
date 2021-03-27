df2b696c5301c814bde82423b39750a521eecb5a
Qasim Hafeez <qasimh556@gmail.com>
2021-03-27 10:27:18 +0400

    authorize roles

--- routes/courses.js
+++ routes/courses.js
@@ -12,7 +12,7 @@ const Course = require("../models/Course");
 const advancedResults = require("../middleware/advancedResults");
 
 // Middlewares
-const { protect } = require("../middleware/auth");
+const { protect, authorize } = require("../middleware/auth");
 
 const router = express.Router({ mergeParams: true });
 
@@ -25,11 +25,11 @@ router
     }),
     getCourses
   )
-  .post(protect, addCourse);
+  .post(protect, authorize("publisher", "user"), addCourse);
 router
   .route("/:id")
   .get(getCourse)
-  .put(protect, updateCourse)
-  .delete(protect, deleteCourse);
+  .put(protect, authorize("publisher", "user"), updateCourse)
+  .delete(protect, authorize("publisher", "user"), deleteCourse);
 
 module.exports = router;
