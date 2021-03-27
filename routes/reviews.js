48387d6827191271a775f0185bfbca8b9121fc3c
Qasim Hafeez <qasimh556@gmail.com>
2021-03-27 17:43:58 +0400

    review routes --complete

---
+++ routes/reviews.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const {
+  getReviews,
+  getReview,
+  addReview,
+  updateReview,
+  deleteReview,
+} = require("../controllers/reviews");
+
+// Models
+const Review = require("../models/Review");
+
+// Initialize route
+const router = express.Router({ mergeParams: true });
+
+// Middlewares
+const advancedResults = require("../middleware/advancedResults");
+const { protect, authorize } = require("../middleware/auth");
+
+// Routes
+router
+  .route("/")
+  .get(
+    advancedResults(Review, {
+      path: "bootcamp",
+      select: "name description",
+    }),
+    getReviews
+  )
+  .post(protect, authorize("user", "admin"), addReview);
+
+router
+  .route("/:id")
+  .get(getReview)
+  .put(protect, authorize("user", "admin"), updateReview)
+  .delete(protect, authorize("user", "admin"), deleteReview);
+
+module.exports = router;
