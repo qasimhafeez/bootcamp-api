f8dc00934295c8cb39c5093b3664274e78394c92
Qasim Hafeez <qasimh556@gmail.com>
2021-03-27 17:44:40 +0400

    security middlewares mount

--- server.js
+++ server.js
@@ -8,14 +8,22 @@ const cookieParser = require("cookie-parser");
 // Load env variables
 dotenv.config({ path: "./config/config.env" });
 
-// importing Middlewares
+// Importing Middlewares
 const morgan = require("morgan");
 const errorHandler = require("./middleware/error");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xssClean = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+
 // Routes
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
 const auth = require("./routes/auth");
 const users = require("./routes/users");
+const reviews = require("./routes/reviews");
 
 // DB
 connectDB();
@@ -25,6 +33,14 @@ const app = express();
 //--------------Middlewares----------------//
 // Body Parser
 app.use(express.json());
+// Express Mongo Sanitize
+app.use(mongoSanitize());
+// Set security headers
+app.use(helmet());
+// Prevent XSS
+app.use(xssClean());
+// hpp for security
+app.use(hpp());
 // Cookie Parser
 app.use(cookieParser());
 // Morgan(color)
@@ -33,6 +49,14 @@ if (process.env.NODE_ENV === "development") {
 }
 // File Upload
 app.use(fileUpload());
+// Req rate limit
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100,
+});
+app.use(limiter);
+// Enable Cors
+app.use(cors());
 // Set Static Folder
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -41,6 +65,7 @@ app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/auth/users", users);
+app.use("/api/v1/reviews", reviews);
 
 app.use(errorHandler);
 
